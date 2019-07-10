bdc67339ed21a3b48d313c47816f7807a369105c
SCAUZSJ <895104160@qq.com>
2019-07-10 20:43:00 +0800

    rename the variable.

--- src/best-charge.js
+++ src/best-charge.js
@@ -4,7 +4,7 @@ function bestCharge(selectedItems) {
   let free = 0;
   let items = loadAllItems();
   let halfPriceItems = [];
-  let output = `============= 订餐明细 =============\n`;
+  let receipt = `============= 订餐明细 =============\n`;
   selectedItems.forEach(item => {
     let arr = item.split(" x ");
     let itemObj = items.find((obj) => {
@@ -16,18 +16,18 @@ function bestCharge(selectedItems) {
       free += itemObj.price * arr[1] / 2;
       halfPriceItems.push(itemObj.name);
     }
-    output += `${itemObj.name} x ${arr[1]} = ${itemObj.price * arr[1]}元\n`;
+    receipt += `${itemObj.name} x ${arr[1]} = ${itemObj.price * arr[1]}元\n`;
   });
-  output += `-----------------------------------\n`;
+  receipt += `-----------------------------------\n`;
   if ((total >= 30 && free > 6) || (total < 30 && free != 0)) {
     total -= free;
-    output += `使用优惠:\n指定菜品半价(${halfPriceItems.join('，')})，省${free}元\n-----------------------------------\n`
+    receipt += `使用优惠:\n指定菜品半价(${halfPriceItems.join('，')})，省${free}元\n-----------------------------------\n`
   } else if (total > 30 && free <= 6) {
     total -= 6;
-    output += `使用优惠:\n满30减6元，省6元\n-----------------------------------\n`
+    receipt += `使用优惠:\n满30减6元，省6元\n-----------------------------------\n`
   }
-  output += `总计：${parseInt(total)}元\n===================================`
-  return output;
+  receipt += `总计：${parseInt(total)}元\n===================================`
+  return receipt;
 }
 //判断该菜是否有半价优惠   1分钟
 function isHalfPriceOfItem(id) {
